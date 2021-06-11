c7def01dbc50d239d66a3b5a2ddc227f3e57194d
Eugeniy Dobrokhvalov <evgeniy.dobrokhvalov@geosteertech.com>
2021-06-12 00:00:13 +0300

    Fill custom data

--- apps/blog/gatsby-config.js
+++ apps/blog/gatsby-config.js
@@ -1,45 +1,19 @@
-module.exports = {
-  siteMetadata: {
-    title: `blog`,
-    description: `This is a gatsby application created by Nx.`,
-  },
-  plugins: [
-    'gatsby-plugin-sass',
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
 
-    {
-      resolve: 'gatsby-plugin-svgr',
-      options: {
-        svgo: false,
-        ref: true,
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    {
-      resolve: require.resolve(`@nrwl/gatsby/plugins/nx-gatsby-ext-plugin`),
-      options: {
-        path: __dirname,
-      },
-    },
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `blog`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/logo.svg`,
-      },
-    },
-  ],
+const config = require('./config');
+const plugins = require('./gatsby-config.plugins');
+
+module.exports = {
+  pathPrefix: config.pathPrefix,
+  siteMetadata: {
+    // Data used by some gatsby plugins
+    siteUrl: config.siteUrl,
+    title: config.siteTitle,
+    description: config.siteDescription,
+  },
+  plugins,
 };
